477a2802f5bba5629464b17a462d2f2136ccae94
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-07-14 11:55:52 -0500

    feat: add UUID dependency and implement map controls with zoom and marker functionalities

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
@@ -1,24 +1,63 @@
-import { AfterViewInit, Component, viewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  effect,
+  ElementRef,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { environment } from '../../../environments/environment';
+import mapboxgl, { ScaleControl } from 'mapbox-gl';
+import { DecimalPipe, JsonPipe } from '@angular/common';
 
 mapboxgl.accessToken = environment.mapboxKey;
 @Component({
   selector: 'app-fullscreen-map-page',
-  imports: [],
+  imports: [DecimalPipe, JsonPipe],
   templateUrl: './fullscreen-map-page.html',
   styleUrl: './fullscreen-map-page.css',
 })
 export class FullscreenMapPage implements AfterViewInit {
-  divElement = viewChild('map');
+  divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null);
+  zoom = signal(14);
+  coordinates = signal({
+    lng: -75.6892359,
+    lat: 4.8051041,
+  });
+
+  zoomEffect = effect(() => {
+    if (!this.map()) return;
+    this.map()?.zoomTo(this.zoom());
+  });
 
   async ngAfterViewInit() {
-    if (!this.divElement()) return;
-const element = this.divElement().nativeElement;
+    if (!this.divElement()?.nativeElement) return;
+    await new Promise((resolve) => setTimeout(resolve, 80));
+    const element = this.divElement()!.nativeElement;
+    const { lat, lng } = this.coordinates();
     const map = new mapboxgl.Map({
-      container: this.divElement.nativeElement,
+      container: element,
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [-74.5, 40],
-      zoom: 9,
+      center: [lng, lat],
+      zoom: this.zoom(),
     });
+
+    this.mapListeners(map);
+  }
+
+  mapListeners(map: mapboxgl.Map) {
+    map.on('zoomend', (event) => {
+      const newZoom = event.target.getZoom();
+      this.zoom.set(newZoom);
+    });
+    map.on('moveend', () => {
+      const center = map.getCenter();
+      this.coordinates.set(center);
+    });
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+    map.addControl(new mapboxgl.FullscreenControl(), 'top-right');
+    map.addControl(new ScaleControl(), 'bottom-left');
+    this.map.set(map);
   }
 }
--- src/app/pages/markers-page/markers-page.ts
+++ src/app/pages/markers-page/markers-page.ts
@@ -1,11 +1,103 @@
-import { Component } from '@angular/core';
-
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  signal,
+  viewChild,
+} from '@angular/core';
+import mapboxgl, { LngLatLike } from 'mapbox-gl';
+import { environment } from '../../../environments/environment';
+import { v4 as UUIDV4 } from 'uuid';
+import { JsonPipe } from '@angular/common';
+mapboxgl.accessToken = environment.mapboxKey;
+interface Marker {
+  id: string;
+  mapboxMarker: mapboxgl.Marker;
+}
 @Component({
   selector: 'app-markers-page',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './markers-page.html',
-  styleUrl: './markers-page.css'
+  styleUrl: './markers-page.css',
 })
-export class MarkersPage {
+export class MarkersPage implements AfterViewInit {
+  divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null);
+  markers = signal<Marker[]>([]);
+  async ngAfterViewInit() {
+    if (!this.divElement()?.nativeElement) return;
+    await new Promise((resolve) => setTimeout(resolve, 80));
+    const element = this.divElement()!.nativeElement;
 
+    const map = new mapboxgl.Map({
+      container: element,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-75.8835186, 4.9036393],
+      zoom: 14,
+    });
+
+    // const marker = new mapboxgl.Marker({ draggable: true, color: 'green' })
+    //   .setLngLat([-75.8835186, 4.9036393])
+    //   .addTo(map);
+    // marker.on('dragend', () => {
+    //   const lngLat = marker.getLngLat();
+    //   console.log('Marker dragged to:', lngLat);
+    // });
+    this.mapListeners(map);
+  }
+
+  mapListeners(map: mapboxgl.Map) {
+    map.on('click', (event) => {
+      this.mapClick(event);
+    });
+
+    this.map.set(map);
+  }
+
+  mapClick(event: mapboxgl.MapMouseEvent) {
+    if (!this.map()) return;
+    const map = this.map()!;
+    // Genera un color aleatorio agradable usando HSL para tonos vibrantes y suaves
+    const hue = Math.floor(Math.random() * 360); // cualquier tono
+    const saturation = 60 + Math.floor(Math.random() * 20); // 60-80% para colores vivos
+    const lightness = 60 + Math.floor(Math.random() * 15); // 60-75% para suavidad
+    const alpha = 0.85; // ligeramente translúcido
+
+    const color = `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`;
+    const coords = event.lngLat;
+    const mapboxMarker = new mapboxgl.Marker({
+      color,
+    })
+      .setLngLat(coords)
+      .addTo(map);
+    const newMarker: Marker = {
+      id: UUIDV4(),
+      mapboxMarker,
+    };
+    this.markers.update((markers) => [newMarker, ...markers]);
+  }
+
+  flyToMarker(lngLat: LngLatLike) {
+    if (!this.map()) return;
+    this.map()!.flyTo({
+      center: lngLat,
+      zoom: 16,
+      speed: 1.2,
+      curve: 1,
+      easing(t) {
+        return t;
+      },
+    });
+  }
+
+  deleteMarker(marker: Marker) {
+    if (!this.map()) return;
+
+    const map = this.map()!;
+
+    marker.mapboxMarker.remove();
+    this.markers.update((markers) => {
+      return markers.filter((m) => m.id !== marker.id);
+    });
+  }
 }
